e2e74d28c657e5d7473c93a8b472484079a38bb5
Pannala  Rajasri <rajasri.p@bourntec.in>
2022-05-13 12:08:48 +0530

    first commit

---
+++ main/java/com/bourntec/aaplearning/AaplearningApplication.java
@@ -0,0 +1,13 @@
+package com.bourntec.aaplearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaplearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaplearningApplication.class, args);
+	}
+
+}
