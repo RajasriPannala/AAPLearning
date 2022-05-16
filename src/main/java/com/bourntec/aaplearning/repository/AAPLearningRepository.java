253486003316be2e76f2dfe03938eac73d36783f
PannalaRajasri <PannalaRajasri@BRNHYD0122L035>
2022-05-16 15:52:18 +0530

    Third commit

--- src/main/java/com/bourntec/aaplearning/repository/AAPLearningRepository.java
+++
@@ -1,5 +0,0 @@
-package com.bourntec.aaplearning.repository;
-
-public interface AAPLearningRepository {
-
-}
--- src/main/java/com/bourntec/aaplearning/service/impl/AAPLearningServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.bourntec.aaplearning.service.impl;
-
-public class AAPLearningServiceImpl {
-
-}
