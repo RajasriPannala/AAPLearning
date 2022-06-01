1db11047943ea09f2df58b2d9f176689cd3604cc
Jeenathomas1998 <jeenathomas1998@gmail.com>
2022-06-01 17:12:23 +0530

    implemented jwt token

--- src/main/java/com/bourntec/aaplearning/modules/commonmanagement/CommonManagamentApplication.java
+++ src/main/java/com/bourntec/aaplearning/modules/commonmanagement/CommonManagamentApplication.java
@@ -3,6 +3,7 @@ package com.bourntec.aaplearning.modules.commonmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -15,6 +16,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 //@EnableEurekaClient
 
 @EntityScan("com.bourntec.aaplearning.entity")
+@ComponentScan({"com.bourntec.aaplearning.modules.commonmanagement","com.bourntec.aaplearning.utility"})
 public class CommonManagamentApplication {
 
 	public static void main(String[] args) {
