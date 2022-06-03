d48d0ded8dac29c984f9e2e1b005871797a8c18e
Jeenathomas1998 <jeenathomas1998@gmail.com>
2022-06-03 13:18:33 +0530

    mockito junit test added

---
+++ src/test/java/com/bourntec/aaplearning/AAPLearningProjTest.java
@@ -0,0 +1,13 @@
+package com.bourntec.aaplearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AAPLearningProjTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
