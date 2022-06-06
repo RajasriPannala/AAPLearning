cecf958aafc822ab20e0ba2d772295894da4f427
ESTHER <esther.tomy@bourntec.com>
2022-06-06 11:45:05 +0530

    conflict fixed

--- src/main/java/com/bourntec/aaplearning/modules/commonmanagement/CommonManagamentApplication.java
+++ src/main/java/com/bourntec/aaplearning/modules/commonmanagement/CommonManagamentApplication.java
@@ -3,7 +3,7 @@ package com.bourntec.aaplearning.modules.commonmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-
+import org.springframework.context.annotation.ComponentScan;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
@@ -11,10 +11,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 
 
-
 //@EnableEurekaClient
 
 @EntityScan("com.bourntec.aaplearning.entity")
+@ComponentScan({"com.bourntec.aaplearning.modules.commonmanagement","com.bourntec.aaplearning.utility"})
 public class CommonManagamentApplication {
 
 	public static void main(String[] args) {
