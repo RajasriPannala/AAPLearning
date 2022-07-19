51d742cfd696a7e75629ca6f7944ee9edefcdd50
Rohini <rohini.pm@bourntec.com>
2022-07-19 18:26:47 +0530

    conflicts fixed

--- src/main/java/com/bourntec/aaplearning/modules/invoicemanagement/v1/controller/InvoiceCustomController.java
+++ src/main/java/com/bourntec/aaplearning/modules/invoicemanagement/v1/controller/InvoiceCustomController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bourntec.aaplearning.modules.invoicemanagement.v1.service.CsvOperationService;
 import com.bourntec.aaplearning.modules.invoicemanagement.v1.service.InvoiceService;
 @RestController
 
@@ -11,8 +12,11 @@ public class InvoiceCustomController {
 	
 	@Autowired
 	InvoiceService invoiceService;
+	CsvOperationService csvOperationService;
+	
 	@GetMapping("/download/csv")
 	public void downloadAsCsv() {
 		invoiceService.downloadAsCsv();
+		
 	}
 }
